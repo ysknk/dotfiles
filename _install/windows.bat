47d827addbcf3cbaf564c6133deb875736253bbf
ysknk <31568245+ysknk@users.noreply.github.com>
2019-06-28 19:02:26 +0900

    Update windows.bat

--- _install/windows.bat
+++ _install/windows.bat
@@ -6,12 +6,16 @@ if exist .vimrc (
   echo F | xcopy /Y /D /R .vimrc ..\_vimrc
 )
 
+if exist .gvimrc (
+  echo F | xcopy /Y /D /R .gvimrc ..\_gvimrc
+)
+
 if exist .vimrc (
-  echo F | xcopy /Y /D /R .vimrc ..\.config\nvim\init.vim
+  echo F | xcopy /Y /D /R .vimrc ..\AppData\Local\nvim\init.vim
 )
 
 if exist .gvimrc (
-  echo F | xcopy /Y /D /R .gvimrc ..\_gvimrc
+  echo F | xcopy /Y /D /R .gvimrc ..\AppData\Local\nvim\ginit.vim
 )
 
 if exist .deinrc (
