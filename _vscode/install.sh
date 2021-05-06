e42b8b8e6f4f93dfce71551a9c1ca6a6f45a9b86
ysknk <31568245+ysknk@users.noreply.github.com>
2021-05-06 17:59:54 +0900

    Create install.sh

---
+++ _vscode/install.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+SCRIPT_DIR=$(cd $(dirname $0) && pwd)
+VSCODE_SETTING_DIR=~/Library/Application\ Support/Code/User
+
+rm "$VSCODE_SETTING_DIR/settings.json"
+ln -s "$SCRIPT_DIR/settings.json" "${VSCODE_SETTING_DIR}/settings.json"
+
+rm "$VSCODE_SETTING_DIR/keybindings.json"
+ln -s "$SCRIPT_DIR/keybindings.json" "${VSCODE_SETTING_DIR}/keybindings.json"
+
+# install extention
+cat extensions.txt | while read line
+do
+ code --install-extension $line
+done
